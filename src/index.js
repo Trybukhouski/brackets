eb792ebebc249052062e5204f8abe54bc9e80def
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-03-08 00:02:16 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const joinBracketConfig = bracketsConfig.map(el => el.join(''));
+  for (let i = 0; i < joinBracketConfig.length; i++) {
+    !str.includes(joinBracketConfig[i]) ? str : (str = str.replace(joinBracketConfig[i], '')) && (i = -1);
+  }
+  return str.length === 0;
 }
